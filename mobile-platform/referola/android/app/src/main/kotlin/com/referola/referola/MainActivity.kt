0bed1dfbde466eb2c474ce71894f6803fcee1ff8
R P S Naik <naik.rpsn@gmail.com>
2020-07-20 13:20:33 +0530

    initial commit

---
+++ mobile-platform/referola/android/app/src/main/kotlin/com/referola/referola/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.referola.referola
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ mobile-platform/referola_businesses/android/app/src/main/kotlin/com/referola/referola_businesses/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.referola.referola_businesses
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
